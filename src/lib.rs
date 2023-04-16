532eeb68b6255fbd6cc5abc8a6ebc6056505fc21
nuttyCream <issanutty@gmail.com>
2023-04-15 22:35:13 -0700

    Added simple read save test and sample game files

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,34 @@ pub use app::App;
 pub use modding::view::ModdingView;
 pub use save_editing::view::SaveEditingView;
 
-pub trait View {
-    fn ui(&mut self, ui: &mut egui::Ui);
+#[cfg(test)]
+mod tests {
+    use crate::save_editing;
+
+    #[test]
+    fn test_read_save() {
+        let save_dir = std::env::current_dir()
+            .unwrap()
+            .join("src")
+            .join("tests")
+            .join("saves");
+
+        for entry in std::fs::read_dir(&save_dir).unwrap() {
+            let entry = entry.unwrap();
+            let file_name = entry.file_name().into_string().unwrap();
+
+            if file_name.ends_with(".xml") {
+                let save_name = file_name.clone();
+
+                let result = save_editing::parser::read_save(save_name, &save_dir);
+
+                assert!(result.is_ok());
+
+                let game_save = result.unwrap();
+
+                // Add additional tests for each field of the GameSave struct
+                assert_eq!(game_save.name, file_name);
+            }
+        }
+    } 
 }
